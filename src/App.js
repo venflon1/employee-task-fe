86aee3d988dd7fda409c45dbd84791b889b40173
venflon1 <roberto.amato.11.93@gmail.com>
2022-12-19 10:23:26 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./styles.css";
+import ComponentA from "./components/ComponentA";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello React</h1>
+      <ComponentA name="Pipp" />
     </div>
   );
 }
+
+export default App;
---
+++ src/components/ComponentA.js
@@ -0,0 +1,5 @@
+const ComponentA = (props) => {
+    return <h1>Sono un ComponentA e mi chiamo {props.name}</h1>;
+}
+
+export default ComponentA;
